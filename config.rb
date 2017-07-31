b9d1e1b136140769249d9f36a4d1b10babdc7ef4
Fernando Morgado <github@fernandomorgado.eu>
2017-07-31 16:48:33 +0100

    finished Flat dynamic view exercise

--- config.rb
+++ config.rb
@@ -20,3 +20,7 @@ activate :deploy do |deploy|
   deploy.build_before = true
   deploy.deploy_method = :git
 end
+
+["talesunited", "hamood93", "gnovo"].each do |name|
+  proxy "/flats/#{name}.html", "/flats/show.html", locals: { owner: name }, ignore: true
+end
